13f7969b5f3f98892cf66def21ca19a839d5e5d8
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-03-13 20:26:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("Hello World!");
+console.log("Hey,I am running")
+console.log("Hey,I am  also running")
+console.log("Hey,I also  running please")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want your computer to blast")
+if(isTrue){
+    console.log("Computer is blasting")
+
+}
+else{
+    console.log("computer is not blasting")
+}
+console.log("your number is "+a)
+
+document.title = "Hey I am good"
+
+document.body.style.backgroundColor = "red"
