b1181c8626bb7b322e33180e4e117ac3af377679
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-03-14 17:10:46 +0530

    Add files via upload

---
+++ video55/script.js
@@ -0,0 +1,28 @@
+console.log("hey! tutorial  of 2")
+
+var a=10;
+var b=43;
+var c= "kumar"
+console.log(a+b)
+console.log(a+b+87)
+console.log(typeof a,typeof b,typeof c)
+
+let x= "Raj";
+let y= 22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r = null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o ={
+    name:"Kumar Raj",
+    "job code " :"1776109",
+    "is_handsome" :true
+}
+console.log(o);
+o.office = "Deloitee";
+console.log(o);
+o.office="Google"
+console.log(o);
